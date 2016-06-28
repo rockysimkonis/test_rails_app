80fcbc953564391d8ad165bae56ef6c7741c1558
Rocky Simkonis <rokas.simkonis@gmail.com>
2016-06-28 17:16:08 -0500

    Some basic sample pages

---
+++ app/controllers/concerns/welcome_controller.rb
@@ -0,0 +1,9 @@
+class WelcomeController < ApplicationController
+  def home
+    #code
+  end
+
+  def about
+    #code
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/home', to: 'welcome#home'
+  get 'welcome/about', to: 'welcome#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
